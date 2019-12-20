1c249e6ad7a571722773219e5a1ec24cc58380d3
peter bohac <pbohac@apple.com>
2019-12-20 09:07:44 -0800

    Minor re-factor / clean-up of Day 20

--- Day 20/Day 20/main.swift
+++ Day 20/Day 20/main.swift
@@ -37,30 +37,59 @@ extension Dictionary where Key == Coordinate {
     var yRange: ClosedRange<Int> { keys.map { $0.y }.range() }
 }
 
-func BFSFindShortestDistance<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [Node]?)) -> Int? {
-    var visited: [Node: Int] = [:]
-    var queue: [(node: Node, steps: Int)] = [(start, 0)]
+struct BFS {
+    static func findShortestPath<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [Node]?)) -> [Node]? {
+        typealias Path = [Node]
+        var visited: [Node: Path] = [:]
+        var queue: [(node: Node, path: Path)] = [(start, [])]
 
-    while queue.isEmpty == false {
-        var (node, steps) = queue.removeFirst()
-        guard let nextNodes = getNextNodes(node) else {
-            return steps
-        }
-        steps += 1
-        for nextNode in nextNodes {
-            if let previousSteps = visited[nextNode], previousSteps <= steps {
-                continue
+        while queue.isEmpty == false {
+            var (node, path) = queue.removeFirst()
+            guard let nextNodes = getNextNodes(node) else {
+                return path
             }
-            if queue.contains(where: { $0.node == nextNode } ) {
-                continue
+            path.append(node)
+            for nextNode in nextNodes {
+                if let previousPath = visited[nextNode], previousPath.count <= path.count {
+                    continue
+                }
+                if queue.contains(where: { $0.node == nextNode } ) {
+                    continue
+                }
+                queue.append((nextNode, path))
             }
-            queue.append((nextNode, steps))
+            visited[node] = path
         }
-        visited[node] = steps
+
+        // No possible path exists
+        return nil
     }
 
-    // No possible path exists
-    return nil
+    static func findShortestDistance<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [Node]?)) -> Int? {
+        var visited: [Node: Int] = [:]
+        var queue: [(node: Node, steps: Int)] = [(start, 0)]
+
+        while queue.isEmpty == false {
+            var (node, steps) = queue.removeFirst()
+            guard let nextNodes = getNextNodes(node) else {
+                return steps
+            }
+            steps += 1
+            for nextNode in nextNodes {
+                if let previousSteps = visited[nextNode], previousSteps <= steps {
+                    continue
+                }
+                if queue.contains(where: { $0.node == nextNode } ) {
+                    continue
+                }
+                queue.append((nextNode, steps))
+            }
+            visited[node] = steps
+        }
+
+        // No possible path exists
+        return nil
+    }
 }
 
 func loadMaze(from data: [[String]]) -> (start: Coordinate, end: Coordinate, portals: [Coordinate: Coordinate]) {
@@ -116,7 +145,7 @@ func loadMaze(from data: [[String]]) -> (start: Coordinate, end: Coordinate, por
 func part1(_ data: [[String]]) {
     let (start, end, portals) = loadMaze(from: data)
 
-    let steps = BFSFindShortestDistance(from: start) { coord in
+    let steps = BFS.findShortestDistance(from: start) { coord in
         if coord == end {
             return nil
         }
@@ -134,30 +163,60 @@ part1(InputData.challenge)
 
 // MARK: Part 2
 
-func AStarFindShortestDistance<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [(node: Node, cost: Int)]?)) -> Int? {
-    var visited: [Node: Int] = [:]
-    var queue: [Node: Int] = [start: 0]
+struct AStar {
+    static func findShortestPath<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [(node: Node, cost: Int)]?)) -> ([Node], Int)? {
+        typealias Path = [Node]
+        var visited: [Node: Int] = [:]
+        var queue: [Node: (path: Path, cost: Int)] = [start: ([], 0)]
 
-    while let (node, currentCost) = queue.min(by: { $0.value < $1.value}) {
-        queue.removeValue(forKey: node)
-        guard let nextNodes = getNextNodes(node) else {
-            return currentCost
-        }
-        for (nextNode, cost) in nextNodes {
-            let newCost = currentCost + cost
-            if let previousCost = visited[nextNode], previousCost <= newCost {
-                continue
+        while let (node, (path, currentCost)) = queue.min(by: { $0.value.cost < $1.value.cost}) {
+            queue.removeValue(forKey: node)
+            guard let nextNodes = getNextNodes(node) else {
+                return (path + [node], currentCost)
             }
-            if let queuedCost = queue[nextNode], queuedCost <= newCost {
-                continue
+            let newPath = path + [node]
+            for (nextNode, cost) in nextNodes {
+                let newCost = currentCost + cost
+                if let previousCost = visited[nextNode], previousCost <= newCost {
+                    continue
+                }
+                if let queued = queue[nextNode], queued.cost <= newCost {
+                    continue
+                }
+                queue[nextNode] = (newPath, newCost)
             }
-            queue[nextNode] = newCost
+            visited[node] = currentCost
         }
-        visited[node] = currentCost
+
+        // No possible path exists
+        return nil
     }
 
-    // No possible path exists
-    return nil
+    static func findShortestDistance<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [(node: Node, cost: Int)]?)) -> Int? {
+        var visited: [Node: Int] = [:]
+        var queue: [Node: Int] = [start: 0]
+
+        while let (node, currentCost) = queue.min(by: { $0.value < $1.value}) {
+            queue.removeValue(forKey: node)
+            guard let nextNodes = getNextNodes(node) else {
+                return currentCost
+            }
+            for (nextNode, cost) in nextNodes {
+                let newCost = currentCost + cost
+                if let previousCost = visited[nextNode], previousCost <= newCost {
+                    continue
+                }
+                if let queuedCost = queue[nextNode], queuedCost <= newCost {
+                    continue
+                }
+                queue[nextNode] = newCost
+            }
+            visited[node] = currentCost
+        }
+
+        // No possible path exists
+        return nil
+    }
 }
 
 extension Collection {
@@ -191,10 +250,7 @@ struct Pair<T: Hashable & Comparable>: Hashable {
     }
 }
 
-var outerX = [0, 0]
-var outerY = [0, 0]
-
-func loadMaze2(from data: [[String]]) -> (portals: [String: Coordinate], distances: [Pair<String>: Int]) {
+func loadMaze2(from data: [[String]]) -> (portals: [String: (coord: Coordinate, isOuter: Bool)], distances: [Pair<String>: Int]) {
     var grid: [Coordinate: String] = [:]
     data.enumerated().forEach { y, row in
         row.enumerated().forEach { x, char in
@@ -203,8 +259,11 @@ func loadMaze2(from data: [[String]]) -> (portals: [String: Coordinate], distanc
         }
     }
 
+    let outerX = [grid.xRange.min()! + 2, grid.xRange.max()! - 2]
+    let outerY = [grid.yRange.min()! + 2, grid.yRange.max()! - 2]
+
     let alphabet = "abcdefghijklmnopqrstuvwxyz".uppercased()
-    var portals: [String: Coordinate] = [:]
+    var portals: [String: (coord: Coordinate, isOuter: Bool)] = [:]
     for y in grid.yRange {
         for x in grid.xRange {
             let coord = Coordinate(x: x, y: y)
@@ -221,27 +280,25 @@ func loadMaze2(from data: [[String]]) -> (portals: [String: Coordinate], distanc
                 tuple = (grid[coord.up, default: " "] + char, coord.down)
             }
             if let tuple = tuple {
+                let isOuter = outerX.contains(tuple.coord.x) || outerY.contains(tuple.coord.y)
                 if tuple.id == "AA" || tuple.id == "ZZ" {
-                    portals[tuple.id] = tuple.coord
+                    portals[tuple.id] = (tuple.coord, isOuter)
                 } else {
                     if portals[tuple.id + "1"] != nil {
-                        portals[tuple.id + "2"] = tuple.coord
+                        portals[tuple.id + "2"] = (tuple.coord, isOuter)
                     } else {
-                        portals[tuple.id + "1"] = tuple.coord
+                        portals[tuple.id + "1"] = (tuple.coord, isOuter)
                     }
                 }
             }
         }
     }
 
-    outerX = [grid.xRange.min()! + 2, grid.xRange.max()! - 2]
-    outerY = [grid.yRange.min()! + 2, grid.yRange.max()! - 2]
-
     var distanceFromPortalToPortal: [Pair<String>: Int] = [:]
     for p in portals.keys.pick(2) {
         let pair = Pair(p[0], p[1])
-        if let distance = BFSFindShortestDistance(from: portals[pair.a]!, using: { coord in
-            if coord == portals[pair.b]! { return nil }
+        if let distance = BFS.findShortestDistance(from: portals[pair.a]!.coord, using: { coord in
+            if coord == portals[pair.b]!.coord { return nil }
             return coord.neighbours.filter { data[$0.y][$0.x] == "." }
         }) {
             distanceFromPortalToPortal[pair] = distance
@@ -251,37 +308,37 @@ func loadMaze2(from data: [[String]]) -> (portals: [String: Coordinate], distanc
     return (portals, distanceFromPortalToPortal)
 }
 
-func getLevelChangeWhenUsingPortal(at coord: Coordinate) -> Int {
-    return outerX.contains(coord.x) || outerY.contains(coord.y) ? -1 : 1
-}
+func part2(_ data: [[String]]) {
+    let (portals, distances) = loadMaze2(from: data)
+    let portalIds = Array(portals.keys)
 
-let (portals, distances) = loadMaze2(from: InputData.challenge)
-let portalIds = Array(portals.keys)
-
-struct State: Hashable {
-    var location: String
-    var level: Int
-}
-
-let start = State(location: "AA", level: 0)
-let end = State(location: "ZZ", level: 0)
-
-let steps = AStarFindShortestDistance(from: start) { state in
-    if state == end {
-        return nil
+    struct State: Hashable {
+        var location: String
+        var level: Int
     }
-    let reachablePortals = portalIds.filter { distances[Pair(state.location, $0)] != nil }
-    return reachablePortals.compactMap { portal -> (State, Int)? in
-        if portal == "AA" { return nil }
-        let distance = distances[Pair(state.location, portal)]!
-        if portal == "ZZ" {
-            return state.level == 0 ? (State(location: "ZZ", level: 0), distance) : nil
+
+    let start = State(location: "AA", level: 0)
+    let end = State(location: "ZZ", level: 0)
+
+    let steps = AStar.findShortestDistance(from: start) { state in
+        if state == end {
+            return nil
         }
-        let nextLevel = state.level + getLevelChangeWhenUsingPortal(at: portals[portal]!)
-        guard nextLevel >= 0 else { return nil }
-        let nextLocation = String(portal.hasSuffix("1") ? portal.prefix(2) + "2" : portal.prefix(2) + "1")
-        return (State(location: nextLocation, level: nextLevel), distance + 1)
-    }
-}!
+        let reachablePortals = portalIds.filter { distances[Pair(state.location, $0)] != nil }
+        return reachablePortals.compactMap { portal -> (State, Int)? in
+            if portal == "AA" { return nil }
+            let distance = distances[Pair(state.location, portal)]!
+            if portal == "ZZ" {
+                return state.level == 0 ? (State(location: "ZZ", level: 0), distance) : nil
+            }
+            let nextLevel = state.level + (portals[portal]!.isOuter ? -1 : 1)
+            guard nextLevel >= 0 else { return nil }
+            let nextLocation = String(portal.hasSuffix("1") ? portal.prefix(2) + "2" : portal.prefix(2) + "1")
+            return (State(location: nextLocation, level: nextLevel), distance + 1)
+        }
+    }!
 
-print("Distance: \(steps)")
+    print("Distance: \(steps)")
+}
+
+part2(InputData.challenge)
